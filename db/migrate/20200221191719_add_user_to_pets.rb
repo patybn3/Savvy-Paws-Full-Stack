05e33aeceab656f5f5aef8553e9f8d9bb38f54eb
patybn3 <paty.nigro@gmail.com>
2020-02-21 14:18:35 -0500

    Add user to Pets table

---
+++ db/migrate/20200221191719_add_user_to_pets.rb
@@ -0,0 +1,5 @@
+class AddUserToPets < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :pets, :user, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_21_191205) do
+ActiveRecord::Schema.define(version: 2020_02_21_191719) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 2020_02_21_191205) do
     t.string "favorite_toy"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_pets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -46,4 +48,5 @@ ActiveRecord::Schema.define(version: 2020_02_21_191205) do
   end
 
   add_foreign_key "examples", "users"
+  add_foreign_key "pets", "users"
 end
