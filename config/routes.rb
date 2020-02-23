8ded63f6214547f7594171eac674080cf824d58f
patybn3 <paty.nigro@gmail.com>
2020-02-23 17:15:03 -0500

    Routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   resources :pets, except: %i[new edit]
   # RESTful routes
   resources :examples, except: %i[new edit]
-
+  # Pets
+  post '/pets' => 'pets#create'
+  get '/pets/:id' => 'pets#show'
+  delete '/pets/:id' => 'pets#destroy'
+  patch '/pets/:id' => 'pets#update'
+  index '/pets' => 'pets#index'
   # Custom routes
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
