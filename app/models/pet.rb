a7b25588de25012ee2ae1552c5c9fedc9a60c5e4
patybn3 <paty.nigro@gmail.com>
2020-02-25 09:18:21 -0500

    Adjustments made to the serializer

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,3 @@
 class Pet < ApplicationRecord
-  belongs_to :current_user, class_name: 'User', foreign_key: 'user_id'
-  has_one :user
+  belongs_to :user
 end
--- app/serializers/pet_serializer.rb
+++ app/serializers/pet_serializer.rb
@@ -1,4 +1,8 @@
 class PetSerializer < ActiveModel::Serializer
   attributes :id, :species, :breed, :name, :dob, :favorite_toy
-  has_one :current_user
+  has_one :user
+
+  def editable
+    scope == object.user
+  end
 end
