5a4e5e22cc581d1b4ca73826138f5328e1716649
patybn3 <paty.nigro@gmail.com>
2020-03-24 19:10:44 -0400

    Changed cors.rb file to allow CORS calls

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,17 +8,18 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-development_client_origin = 'http://localhost:7165' if Rails.env.development?
+# development_client_origin = 'http://localhost:7165' if Rails.env.development?
 
-development_client_origin ||= ENV['CLIENT_ORIGIN']
+# development_client_origin ||= ENV['CLIENT_ORIGIN']
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins do |origin, _env|
-      ENV['CLIENT_ORIGIN'] == '*' ||
-        origin == ENV['CLIENT_ORIGIN'] ||
-        origin == development_client_origin
-    end
+    origins '*'
+    # do |origin, _env|
+    #   ENV['CLIENT_ORIGIN'] == '*' ||
+    #     origin == ENV['CLIENT_ORIGIN'] ||
+    #     origin == development_client_origin
+    # end
     resource '*',
              headers: :any,
              methods: %i[options head get delete patch put post]
